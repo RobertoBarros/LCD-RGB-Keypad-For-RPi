a9b244ad7035170f9368b36d3e9541320eb018eb
Roberto Barros <roberto.f.barros@gmail.com>
2020-04-13 23:26:02 +0100

    RPM graph

--- ets2.py
+++ ets2.py
@@ -6,13 +6,17 @@ from time import sleep
 import copy
 import atexit
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
-import convertAccentCharutf8
 from GlyphSprites import Sprites
 import requests
 
 lcd = Adafruit_CharLCDPlate()
 atexit.register(lcd.stop)
 lcd.backlight(True)
+lcd.createChar(0, Sprites.bar0)
+lcd.createChar(1, Sprites.bar1)
+lcd.createChar(2, Sprites.bar2)
+lcd.createChar(3, Sprites.bar3)
+lcd.createChar(4, Sprites.bar4)
 
 while (True):
   r = requests.get('http://192.168.1.101:25555/api/ets2/telemetry').json();
@@ -33,8 +37,16 @@ while (True):
     rpm_percent = int(truck['engineRpm']/truck['engineRpmMax']*100)
   except ZeroDivisionError:
     rpm_percent = 0
+  # rpm_percent_str = "RPM" + str(rpm_percent).rjust(3) + "%"
 
-  rpm_percent_str = "RPM" + str(rpm_percent).rjust(3) + "%"
+  rpm_percent = 83
+
+  rpm_str = '\x00' if rpm_percent > 50 else ''
+  rpm_str = '\x00\x01' if rpm_percent > 60 else rpm_str
+  rpm_str = '\x00\x01\x02' if rpm_percent > 70 else rpm_str
+  rpm_str = '\x00\x01\x02\x03' if rpm_percent > 80 else rpm_str
+  rpm_str = '\x00\x01\x02\x03\x04' if rpm_percent > 90 else rpm_str
+
 
   try:
    fuel_percent = int(truck['fuel']/truck['fuelCapacity']*100)
@@ -49,5 +61,5 @@ while (True):
 
 
   lcd.clear()
-  lcd.message("%s|%s|%s  %s\n%s   %s"%(speed_limit_str, speed_str, cruise_control_str, gear_str, fuel_percent_str, rpm_percent_str))
-  sleep(0.2)
+  lcd.message("%s|%s|%s  %s\n%s   %s"%(speed_limit_str, speed_str, cruise_control_str, gear_str, fuel_percent_str, rpm_str))
+  sleep(0.1)
